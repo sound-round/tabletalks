504e6691b55fbc6bd6b794d521efc342b9bdc9d2
denis <yudaev1@gmail.com>
2021-10-25 13:30:32 +0300

    Add login and logout

--- tabletalks/base/urls.py
+++ tabletalks/base/urls.py
@@ -3,8 +3,12 @@ from tabletalks.base import views
 
 
 urlpatterns = [
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+
     path('', views.home, name='home'),
     path('table/<str:pk>/', views.table, name='table'),
+
     path('create-table/', views.create_table, name='create-table'),
     path('update-table/<str:pk>/', views.update_table, name='update-table'),
     path('delete-table/<str:pk>/', views.delete_table, name='delete-table'),
--- tabletalks/base/views.py
+++ tabletalks/base/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from tabletalks.base.models import Table, Topic
 from tabletalks.base.forms import TableForm
+
+
+def login_user(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist.')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Wrong username or password.')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
 
 
 def home(request):
