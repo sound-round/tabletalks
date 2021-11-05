32c7ebc8f65e6091794fcdae9185e5d6db0d1757
denis <yudaev1@gmail.com>
2021-11-05 17:47:41 +0300

    Add ALLOWED_HOSTS"

--- tabletalks/settings.py
+++ tabletalks/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'u!jlho7gz8nz%b*5v&nhz(gbuh(%er
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tabletalks.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
