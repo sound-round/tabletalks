d6e4654cfc0736d415effbbc7f551a7fc5c43cd6
denis <yudaev1@gmail.com>
2021-11-08 22:32:42 +0300

    Prepare project to deploy on VPS

--- tabletalks/settings.py
+++ tabletalks/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'u!jlho7gz8nz%b*5v&nhz(gbuh(%er
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['tabletalks.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['168.235.81.228', '127.0.0.1']
 
 
 # Application definition
@@ -79,13 +79,29 @@ WSGI_APPLICATION = 'tabletalks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
+# with open('tabletalks/tabletalks/psql_password', 'r') as file:
+with open(os.path.join(BASE_DIR, 'tabletalks/psql_password')) as file_:
+    psql_password = file_.readline()
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tabletalks',
+        'USER': 'denis',
+        'PASSWORD': psql_password,
+        'HOST': 'localhost',
+        'POST': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
